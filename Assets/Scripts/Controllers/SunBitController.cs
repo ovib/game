860b212200b666e62b02a317aced7cea8a6cd14b
Ovidiu <blajovidiu@protonmail.com>
2022-01-15 18:51:03 +0100

    sun bits generator done

--- Assets/Scripts/Controllers/SunBitController.cs
+++ Assets/Scripts/Controllers/SunBitController.cs
@@ -6,12 +6,23 @@ public class SunBitController : MonoBehaviour
 {
 
     public int minutes = 30;
-    public TimeManager timeManager;
+    private TimeManager timeManager;
+
+    void Start(){
+        timeManager = GameObject.Find("TimeManager").GetComponent<TimeManager>();
+    }
     
     void OnTriggerEnter(Collider other){
         if(other.name == "Character"){
             timeManager.increaseDay(minutes);
              Destroy(transform.parent.gameObject);
+        } else{
+            Debug.Log("Altro oggetto! nome: " + other);
         }
     }
+
+    // private void OnCollisionStay(Collision collision){
+    //     Debug.LogFormat("{0} collision stay: {1}", this, collision.gameObject);
+    // }
+
 }
--- Assets/Scripts/SunBitsGenerator.cs
+++ Assets/Scripts/SunBitsGenerator.cs
@@ -6,7 +6,7 @@ using System;
 public class SunBitsGenerator : MonoBehaviour
 {
     public GameObject sunBitPrefab;
-    public int num = 20;
+    public int num = 10;
     public float minPosX = -88;
     public float maxPosX = 90;
     public float minPosZ = -70;
@@ -15,31 +15,37 @@ public class SunBitsGenerator : MonoBehaviour
 
     GameObject[] generatedSunBits;
 
+    private System.Random rand;
+
     // Start is called before the first frame update
     void Start()
     {
         generatedSunBits = new GameObject[num];
+        rand = new System.Random();
 
         Generate();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void Generate(){
+        int count = 0;
+
+        while(count < num){
+            float posX = GetRandomNumber(minPosX, maxPosX);
+            float posZ = GetRandomNumber(minPosZ, maxPosZ);
+            
+            bool willCollide = Physics.CheckSphere(new Vector3(posX, posY, posZ), 0.8f);
+            
+            if(!willCollide){
+                generatedSunBits[count] = Instantiate(sunBitPrefab, new Vector3(posX, posY, posZ), Quaternion.identity);
+                count++;
+            }
+        }
     }
 
-    public void Generate(){
-        System.Random rand = new System.Random();
-        double xRange = maxPosX - minPosX;
-        double zRange = maxPosZ - minPosZ;
-        Debug.Log("range X= " + xRange + " rangeZ= " + zRange);
-        for(int i = 0; i < num; i++){
-             double sample = rand.Next();
-             double posX = (sample * xRange) + minPosX;
-             double posZ = (sample * zRange) + minPosZ;
-             Debug.Log("x= " + posX + ", y= " + posY + ", z= " + posZ);
-             generatedSunBits[i] = Instantiate(sunBitPrefab, new Vector3((float) posX, posY, (float) posZ), Quaternion.identity);
-        }
+    private float GetRandomNumber(float min, float max){
+        double range = max - min;
+        double sample = rand.NextDouble();
+        double num = (sample * range) + min;
+        return (float) num;
     }
 }
